1cc452a812d39b1bf5ccc77ec5a120ba7407af7a
Anthony Boyd <anthony@finishlinemg.com>
2025-12-17 23:23:57 -0500

    Add logo to login, force dark theme, rebrand to Rumble In Fort Wayne

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -3,7 +3,12 @@
     'description',
 ])
 
-<div class="flex w-full flex-col text-center">
+<div class="flex w-full flex-col text-center items-center">
+    @if(file_exists(public_path('logo.png')))
+        <img src="{{ asset('logo.png') }}" alt="RUMBLE Logo" class="h-24 mb-4">
+    @elseif(file_exists(public_path('logo.svg')))
+        <img src="{{ asset('logo.svg') }}" alt="RUMBLE Logo" class="h-24 mb-4">
+    @endif
     <flux:heading size="xl">{{ $title }}</flux:heading>
     <flux:subheading>{{ $description }}</flux:subheading>
 </div>
